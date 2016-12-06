d387170eb9b2b432007ff45cf9044b4f3549e05c
eeooee <epiphany.compton@gmail.com>
2016-12-06 14:43:26 -0600

    Implemented fix for getParents and nextOfKin.grandparents

--- app.js
+++ app.js
@@ -286,24 +286,24 @@ const start = (objects, getUserSelection, matchingFirstName, matchingLastName, e
     let inputNumber = "1";
     //let inputNumber = prompt("SELECT A NUMBER\n1. Search by first and last name.\n2. Search by characteristics of the person.\n3. Exit");
     switch (inputNumber) {
-    case "1":
-        //let name = "Mattias Madden".split(' ');
-        //let name = 'Ralph Bob'.split(' ');
-        let name = "Joy Madden".split(' ');
-        let person = matchingFirstName(name[0], matchingLastName(name[1], objects, isLastName), isFirstName);
-        getUserSelection(person[0], objects, getDescendants, getFamily, getNextOfKin);
-        //name = prompt("Enter a first and last name to search.").split(" ");
-        break;
-    case "2":
-        //let traits = prompt("Please type your search terms, separated by commas:\n");
-        break;
-    case "3":
-        alert("You have exited the most-wanted search.");
-        exit();
-        break;
-    default:
-        alert("Invalid Selection!");
-        break;
+        case "1":
+            //let name = "Mattias Madden".split(' ');
+            //let name = 'Ralph Bob'.split(' ');
+            let name = "Joey Madden".split(' ');
+            let person = matchingFirstName(name[0], matchingLastName(name[1], objects, isLastName), isFirstName);
+            getUserSelection(person[0], objects, getDescendants, getFamily, getNextOfKin);
+            //name = prompt("Enter a first and last name to search.").split(" ");
+            break;
+        case "2":
+            //let traits = prompt("Please type your search terms, separated by commas:\n");
+            break;
+        case "3":
+            alert("You have exited the most-wanted search.");
+            exit();
+            break;
+        default:
+            alert("Invalid Selection!");
+            break;
     }
     //start();
 };
@@ -311,20 +311,20 @@ const getUserSelection = (object, objects, getDescendants, getFamily, getNextOfK
     let inputNumber = '3';
     //let inputNumber = prompt("SELECT A NUMBER:\r\n1. Find Descendants\r\n2. Find Immediate Family\r\n3. Find Next of Kin");
     switch (inputNumber) {
-    case '1':
-        console.log(getDescendants(object, objects, isChildren, matchingChildren));
-        //responder(getDescendants(object));
-        break;
-    case '2':
-        console.log(getFamily(object, objects, getParents, isParent, matchingParents, isSpouse, matchingSpouse, isChildren, matchingChildren, isNotObject, excludeMatchingObjects));
-        //responder(family(person));
-        break;
-    case '3':
-        console.log(getNextOfKin(object, objects, getParents, isParent, matchingParents, isSpouse, matchingSpouse, isChildren, matchingChildren, getRelatives, isNotObject, excludeMatchingObjects, getFamily));
-        break;
-    default:
-        alert("Invalid Selection!");
-        break;
+        case '1':
+            console.log(getDescendants(object, objects, isChildren, matchingChildren));
+            //responder(getDescendants(object));
+            break;
+        case '2':
+            console.log(getFamily(object, objects, getParents, isParent, matchingParents, isSpouse, matchingSpouse, isChildren, matchingChildren, isNotObject, excludeMatchingObjects));
+            //responder(family(person));
+            break;
+        case '3':
+            console.log(getNextOfKin(object, objects, getParents, isParent, matchingParents, isSpouse, matchingSpouse, isChildren, matchingChildren, getRelatives, isNotObject, excludeMatchingObjects, getFamily));
+            break;
+        default:
+            alert("Invalid Selection!");
+            break;
     }
 };
 const responder = (object) => console.log(object); //alert(object);
@@ -341,10 +341,10 @@ const matchingLastName = (element, objects, isLastName) =>
 const isFirstName = (element, object) => element === object.firstName;
 const matchingFirstName = (element, objects, isFirstName) =>
     objects.filter(o => isFirstName(element, o));
-const getParents = object => {
-    if (object.length <= 0)
-        return [];
-    return object.parents;
+const getParents = (object, objects, parents = []) => {
+    object.parents.forEach(p => parents.push(objects.filter(o => p === o.id)));
+    return parents;
+
 };
 const isParent = (element, indexOfElement, parents, indexOfParent) =>
     element[indexOfElement] === parents[indexOfParent];
@@ -385,7 +385,7 @@ const getFamily =
     (object, objects, getParents, isParent, matchingParents, isSpouse, matchingSpouse, isChildren, matchingChildren, isNotObject, excludeMatchingObjects, allFamilyMembers = []) => {
         allFamilyMembers.spouse = matchingSpouse(object, objects, isSpouse);
         allFamilyMembers.children = matchingChildren(object, objects, isChildren);
-        allFamilyMembers.parents = getParents(object);
+        allFamilyMembers.parents = getParents(object, objects);
         allFamilyMembers.siblings = matchingParents(allFamilyMembers.parents, excludeMatchingObjects(object, objects, isNotObject), isParent);
         return allFamilyMembers;
     };
@@ -397,13 +397,16 @@ const getNextOfKin =
         nextOfKin.parents = family.parents;
         nextOfKin.siblings = family.siblings;
         nextOfKin.grandChildren = getRelatives(nextOfKin.children, objects, isChildren);
-        //nextOfKin.grandParents = findRelatives(nextOfKin.parents, objects, isParent);
+        nextOfKin.grandParents = [];
+        nextOfKin.parents.forEach(object => nextOfKin.grandParents.push(getParents(object, objects)));
         // nextOfKin.siblingsChildren = findRelatives(nextOfKin.siblings, objects, findMatchingSiblings);
         // nextOfKin.parentsSiblings = findRelatives(nextOfKin.parents, objects, findMatchingSiblings);
         // nextOfKin.greatGrandChildren = findRelatives(nextOfKin.grandChildren, objects, isChildren);
         // nextOfKin.greatGrandParents = findRelatives(nextOfKin.grandParents, objects, isParent);
         return nextOfKin;
     };
+
+const tryFamily = (parent, peopleToCompare) => peopleToCompare.parents[0] === parent || peopleToCompare.parents[1];
 const getRelatives =
     (object, objects, association, relatives = []) => {
         if (object.length <= 0)
