a8c9bf1623fe67a49fbeb337a4bc52bb9898ab8b
underwnm <underwnm@gmail.com>
2016-12-08 09:44:46 -0600

    Implement unit test for matchingGender function.

--- test/nick-specs.js
+++ test/nick-specs.js
@@ -1,9 +1,39 @@
 describe(('function isMatch'), () => {
     const isMatch = (element, object) => object == element;
-    it('match 2 == 2', () => {
-    expect(isMatch(2,2)).toBeTruthy();
+
+    it('match the same integer', () => {
+        expect(isMatch(2 ,2)).toBeTruthy();
     });
-    it('match female == female', () => {
-    expect(isMatch('female','female')).toBeTruthy();
+
+    it('match the same string', () => {
+        expect(isMatch('female' ,'female')).toBeTruthy();
+    });
+
+    it('match string to integer', () => {
+        expect(isMatch('1' ,1)).toBeTruthy();
+    });
+
+    it('false on input that does not match', () => {
+        expect(isMatch(2, 1)).toBeFalsy();
+    });
+});
+
+describe(('function matchingGender'), () => {
+    const dataObject = [ { "gender": "male" }, { "gender": "female" }];
+    const isMatch = (element, object) => object == element;
+
+    const matchingGender = (element, objects, isMatch) =>
+        objects.filter(object => isMatch(element, object.gender));
+
+    let expectedResultsOne = [];
+    expectedResultsOne.push(dataObject[0]);
+    it('check if male matches any object', () => {
+        expect(matchingGender('male', dataObject, isMatch)).toEqual(expectedResultsOne);
+    });
+
+    let expectedResultsTwo = [];
+    expectedResultsTwo.push(dataObject[1]);
+    it('check if female matches any object', () => {
+        expect(matchingGender('female', dataObject, isMatch)).toEqual(expectedResultsTwo);
     });
 });
