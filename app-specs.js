39c1d7ff65dce7cd964daeda1553583ea003ee3e
eeooee <epiphany.compton@gmail.com>
2016-12-08 08:53:57 -0600

    Implemented 3 unit tests

--- app-specs.js
+++ app-specs.js
@@ -1,13 +1,26 @@
 /*jshint esversion: 6 */
 describe(('function ConvertStringHeightToInches'), function() {
-    it('convert string height to inches', function() {
-        var convertHeightToInches = function(height) {
+    var convertHeightToInches = function(height) {
             let inchesInFoot = 12;
             let heightArray = height.replace(/[^\d]/g, ' ').split(' ', 2);
             return (+(heightArray[0] * inchesInFoot) + +heightArray[1]);
         };
+    it('convert string height to inches', function() {
+        expect(convertHeightToInches("5'0'")).toEqual(60);
+    });
+    it('converts string height to inches',function(){
+        expect(convertHeightToInches("0")).toBeNaN();
     });
 });
-expect(convertHeightToInches("5'0'")).toEqual(60);
-});
+
+describe(('function getAge'), function(){
+    var getAge = function(ageString) {
+    let controlledAge = Date.parse(ageString);
+    let msInYear = 31556952000;
+    let negativeFix = Date.now();
+    return parseInt(((negativeFix - controlledAge) / msInYear));
+};
+    it('converts a string birthday as (mm/dd/yyyy/) to an age in years', function(){
+        expect(getAge(03/11/1993)).toEqual(23);
+    });
 });
